62af07d834fda17f16d000a6eec4e37295ec8f83
Igor Wawrzyniak <igorwaw@gmail.com>
2023-12-23 11:42:47 +0000

    added home page template with mapael

--- static/map.js
+++ static/map.js
@@ -1,30 +1,74 @@
 $(function () {
+
+
+    var getElemID = function(elem) {
+        // Show element ID
+        return $(elem.node).attr("data-id");
+    };
+
     $("#map-container").mapael({
         map: {
             name: "world_countries",
-            defaultArea: {
-                attrs: {
-                    fill: "#e4e4e4",
-                    stroke: "#fafafa"
-                },
-                attrsHover: {
-                    fill: "#2196F3"
-                }
-            }
+            zoom: {
+                        enabled: true,
+                        init: {
+                            latitude: 45,
+                            longitude: 10,
+                            level: 10
+                        },
+                    },
+                    defaultArea: {
+                        tooltip: {  content: getElemID  },
+                    },
         },
         areas: {
             // Define the actions for each country
-            "FR": {
-                attrs: {
-                    fill: "#4CAF50"
-                },
-                attrsHover: {
-                    fill: "#8BC34A"
-                },
-                tooltip: { content: "France" },
-                href: "france.html" // Specify the URL to redirect to
+            "AT": {
+                attrs: { fill: "#484802"  },
+                href: "/austria"
             },
-            // Add more countries as needed
+            "IE": {
+                attrs: { fill: "#484802"  },
+                href: "/ireland"
+            },
+            "PL": {
+                attrs: { fill: "#484802"  },
+                href: "/poland"
+            },
+            "LT": {
+                attrs: { fill: "#484802"  },
+                href: "/lithuania"
+            },
+            "BE": {
+                attrs: { fill: "#484802"  },
+                href: "/belgium"
+            },
+            "HR": {
+                attrs: { fill: "#484802"  },
+                href: "/croatia"
+            },
+            "CZ": {
+                attrs: { fill: "#484802"  },
+                href: "/czechia"
+            },
+            "DE": {
+                attrs: { fill: "#484802"  },
+                href: "/germany"
+            },
+            "IT": {
+                attrs: { fill: "#484802"  },
+                href: "/italy"
+            },
+            "GB": {
+                attrs: { fill: "#484802"  },
+                href: "/uk"
+            },
+            "ES": {
+                attrs: { fill: "#484802"  },
+                href: "/spain"
+            },
+
         }
     });
 });
+
